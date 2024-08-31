d865d3a5a1e032ed5f9fd8d00331c8a7974eb8d4
Pablo Schlotthauer <126608539+pabloeze91@users.noreply.github.com>
2024-08-31 14:09:50 -0300

    Update urls.py

--- ejercicios_practica/marvel/e_commerce/api/urls.py
+++ ejercicios_practica/marvel/e_commerce/api/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('comic-list/', comic_list_api_view, name="comic_list_api_view"),
     path('comic-retrieve/', comic_retrieve_api_view, name="comic_retrieve_api_view"),
     path('comic-create/', comic_create_api_view, name="comic_create_api_view"),
+    path('comic-list-filtered/', comic_list_filtered_api_view, name="comic_list_filtered_api_view"),
+    path('comic-list-lowest-price/', comic_list_lowest_price_api_view, name="comic_list_lowest_price_api_view"),
+    path('comic-list-highest-price/', comic_list_highest_price_api_view, name="comic_list_highest_price_api_view")
 ]
